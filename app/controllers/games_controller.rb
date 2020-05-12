9811a82d23ba439e4effebcb630f083b826e5c6e
Yoann Vuillermet <yoann.vuillermet@gmail.com>
2020-05-12 11:38:38 +0200

    last changes

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+
+  def new
+    @letters = Array.new(9) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    raise
+  end
+
+end
